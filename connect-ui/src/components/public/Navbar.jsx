bab0e82d9f03e177fce6da82fdf431e80b0c4cbd
behan05 <=>
2025-06-27 05:28:51 +0530

    feat: add basic structure for public navbar

--- connect-ui/src/components/public/Navbar.jsx
+++ connect-ui/src/components/public/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@/MUI/MuiComponents";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return <div>Navbar</div>
 }
 
 export default Navbar;
